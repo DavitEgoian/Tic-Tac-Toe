c5f9260ae6d245fccc0379942ba9a2c26f729e19
DavitEgoian <dato.egoyan@gmail.com>
2025-04-20 11:59:31 +0400

    Main logic of the game added without checking the winner or preventing overriding values

--- board.py
+++ board.py
@@ -1,3 +1,9 @@
-def draw_board():
-    board = '''-------------\n| X | O | X |\n-------------\n| O | O | 6 |\n-------------\n| X | O | X |\n-------------'''
+def draw_board(spots):
+    board = f'''-------------\n| {spots[1]} | {spots[2]} | {spots[3]} |\n-------------\n| {spots[4]} | {spots[5]} | {spots[6]} |\n-------------\n| {spots[7]} | {spots[8]} | {spots[9]} |\n-------------'''
     print(board)
+
+def check_turn(turn):
+    if turn % 2 == 0:
+        return 'O'
+    else:
+        return 'X'
--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-from board import draw_board
+import os
+from random import choice
+
+from board import draw_board, check_turn
+
+spots = {1: '1', 2: '2', 3: '3', 4:'4', 5:'5',
+         6: '6', 7: '7', 8: '8', 9:'9'}
+
+playing = True
+turn = 0
+
+while playing:
+    os.system("cls")
+    draw_board(spots)
+    user_choice = input('').lower()
+    if user_choice == 'quit':
+        playing = False
+        print('You have quited the game.')
+    else:
+        try:
+            user_choice = int(user_choice)
+        except ValueError:
+            print('Please enter a valid number!')
+        else:
+            turn += 1
+            spots.update({int(user_choice): check_turn(turn)})
+
